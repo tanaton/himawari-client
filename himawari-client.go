0b1eefd0457b978045bd5cd8bc35ab28401142ff
tanaton <heiwaboke@gmail.com>
2018-12-21 22:39:27 +0900

    スレッド数を減らした

--- himawari-client.go
+++ himawari-client.go
@@ -41,7 +41,7 @@ func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.InfoLevel)
 
-	NumCPU = runtime.NumCPU()
+	NumCPU = (runtime.NumCPU() / 4) * 3
 	if NumCPU <= 1 {
 		NumCPU = 2
 	}
